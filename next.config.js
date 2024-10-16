7c1b170f62ac0a17028715003489510699e601a6
ironweb-research <backslash.with.n@gmail.com>
2024-10-16 13:11:01 +0800

    update api calling

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /**
  *  @type {import('next').NextConfig}
  */
-const isProdEnv = process.env.NODE_ENV = 'production'
 
 const nextConfig = {
-    basePath: isProdEnv ? '/we_are' : '',
     output: 'export',
     distDir: 'dist',
     images: {
