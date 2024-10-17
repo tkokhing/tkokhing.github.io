6c1a5ea30a8731114248e84ebce62b04cb7fde1b
ironweb-research <backslash.with.n@gmail.com>
2024-10-17 10:49:07 +0800

    refined checkenv.ts

--- src/lib/checkenv.ts
+++ src/lib/checkenv.ts
@@ -1,34 +1,28 @@
 import { REPO_NAME } from '@/lib/constants';
 
-export function useBasePath() {
-  if (process.env.GITHUB_ACTIONS) // `npm run build` in GitHub
-  {
-    console.log("Running in GitHub Actions CI environment");
-    console.log(`${process.env.GITHUB_ACTIONS}`);
-    return `${REPO_NAME}`
-  }
-  else // Running in local environment
-  {
-    // console.log("Running in local environment");
-    console.log(`${process.env.NODE_ENV}`);
+export function useBasePath(): string {
+  const { GITHUB_ACTIONS, NODE_ENV } = process.env;
 
-    if (process.env.NODE_ENV === 'development') //`npn run dev` in local
-    {
-      console.log("Running in local development environment");
-      console.log(`${process.env.NODE_ENV}`);
-      return ''
-    } 
-    else if (process.env.NODE_ENV === 'production')// `npm run build` in local
-    {
-      console.log("Running in local production environment");
-      console.log(`${process.env.NODE_ENV}`);
-      return  ''
-    } 
-    else if (process.env.NODE_ENV === 'test')//  === 'test', `npx serve ./dist`  in local 
-    {
-      console.log("Running in local testing environment");
-      console.log(`${process.env.NODE_ENV}`);
-      return  ''
-    } 
+  if (GITHUB_ACTIONS) {
+    console.log("Running in GitHub Actions CI environment");
+    console.log(GITHUB_ACTIONS);
+    return REPO_NAME;
   }
+
+  console.log(NODE_ENV);
+  switch (NODE_ENV) {
+    case 'development':
+      console.log("Running in local development environment");
+      break;
+    case 'production':
+      console.log("Running in local production environment");
+      break;
+    case 'test':
+      console.log("Running in local testing environment");
+      break;
+    default:
+      console.log("Unknown environment");
+  }
+
+  return '';
 }
