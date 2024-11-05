fac4a1dc0745791bb2c1b520a720c14829786426
ironweb-research <backslash.with.n@gmail.com>
2024-11-05 11:51:12 +0800

    standardize all logo, and its calling

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,13 +1,13 @@
 // import Link from "next/link";
 import Image from "next/image";
-import { REPO_NAME } from "@/lib/constants";
+import { IRONWEB_PROFILE_LOGO_PNG_URL } from "@/lib/constants";
 
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         <Image
-          src={`https://ironweb-research.github.io/${REPO_NAME}/assets/blog/logo/Ironweb_github_profile.gif`}
+          src={IRONWEB_PROFILE_LOGO_PNG_URL}
           alt="Logo"
           width={400} // Adjust the width as needed
           height={400} // Adjust the height as needed
