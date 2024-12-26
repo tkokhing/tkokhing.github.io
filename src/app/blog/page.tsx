e5fd2325cae417438667ba393f45c3a33fefd060
tkokhing <tkokhing@gmail.com>
2024-12-26 12:58:35 +0800

    insert oneheart in topics for testing

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,30 @@
+import Container from "@/app/_components/container";
+import Alert from "@/app/_components/main_frame/alert";
+import { HeroPost } from "@/app/_components/hero-post";
+import { MoreStories } from "@/app/_components/more-stories";
+import { getAllPosts } from "@/lib/api";
+
+export default function Index() {
+  const allPosts = getAllPosts("_posts");
+
+  const heroPost = allPosts[0];
+
+  const morePosts = allPosts.slice(1);
+
+  return (
+    <main>
+      <Alert/>
+      <Container>
+        <HeroPost
+          title={heroPost.title}
+          coverImage={heroPost.coverImage}
+          date={heroPost.date}
+          author={heroPost.author}
+          slug={heroPost.slug}
+          excerpt={heroPost.excerpt}
+        />
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </Container>
+    </main>
+  );
+}
