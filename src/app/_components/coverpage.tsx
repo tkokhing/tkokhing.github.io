cdaf0c0b7adf1399f926dc4e422c58f2efb8ec9c
tkokhing <tkokhing@gmail.com>
2025-02-13 18:00:55 +0800

    redefined coverpage

--- src/app/_components/coverpage.tsx
+++ src/app/_components/coverpage.tsx
@@ -5,14 +5,14 @@ export default function Index() {
     <main>
       <Container>
         <header>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-600 dark:text-inherit">My Story</h1>
+          <div className="mx-auto max-w-7xl px-2 py-6 sm:px-1 lg:px-8">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-inherit">My Story</h1>
           </div>
         </header>
         <div className="space-y-6 py-8 text-base leading-7 text-gray-600 dark:text-inherit">
           <p>Fell in love in programming when I first crosspath with it back in polytechnic during the 90s. Had fair amount of chances to nurture it, during Uni and some projects in the military. Now that I retired after serving for more than two decades, so here I come...</p>
           <h1><strong>Changes that I observed</strong></h1>	
-          <h1>Well ...</h1>	
+          <h2>Well ...</h2>	
           <ul className="space-y-4 ml-4">
           <p>1. Programming or coding - Nobody called it coding back then, or at least not where I am from LOL.</p>
           <p>2. Good program is called "strong algorithm" then.</p>
