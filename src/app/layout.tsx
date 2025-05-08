e7af42f0dd75394f820abc6094dcc91ddff7983a
tkokhing <tkokhing@gmail.com>
2025-05-08 15:04:02 +0800

    major re-factor RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import cn from "classnames";
+import type { Metadata } from "next";
+import type { Viewport } from 'next'
 
 import AlertBar from "@/app/_components/main_frame/alert-bar";
-import Navigationbar from "@/app/_components/main_frame/navi_bar"
+import Navigationbar from "@/app/_components/main_frame/navi_bar";
 import { NavigationProvider } from "./_components/main_frame/NavigationContext";
 import SubpageHeader from "@/app/_components/main_frame/subpage-header";
 import Footer from "@/app/_components/main_frame/footer";
-import { T_ANI_GIF_URL } from "@/lib/constants";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import cn from "classnames";
 import { ThemeSwitcher } from "@/app/_components/main_frame/theme-switcher";
-
-import "@/styles/globals.css";
+import { T_ANI_GIF_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const viewport: Viewport = {
+  themeColor: 'black',
+}
+
 export const metadata: Metadata = {
-  metadataBase: new URL('http://localhost:3000'),
+  metadataBase: new URL('https://tkokhing.github.io/'),
   title: 'tkokhing',
   description: 'This is my blog.',
   openGraph: {
@@ -24,57 +27,38 @@ export const metadata: Metadata = {
     description: 'This is my blog',
     images: [T_ANI_GIF_URL],
   },
-}
+  icons: {
+    icon: [
+      { url: "/img/logo/favicon_32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/img/logo/favicon_16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/img/logo/favicon.ico", type: "image/x-icon" },
+    ],
+    apple: "/img/logo/apple-touch-icon.png",
+    shortcut: "/img/logo/favicon.ico",
+  },
+  manifest: "/img/logo/site.webmanifest",
+  other: {
+    "msapplication-TileColor": "black",
+    "msapplication-config": "/img/logo/browserconfig.xml",
+    "mask-icon": "/img/logo/safari-pinned-tab.svg",
+    "alternate": "/feed.xml",
+  }
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/logo/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/img/logo/favicon_32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/img/logo/favicon_16x16.png"
-        />
-        <link rel="manifest" href="/img/logo/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/img/logo/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <link rel="icon" href="/img/logo/favicon.ico" type="image/x-icon"/>
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          name="msapplication-config"
-          content="/img/logo/browserconfig.xml"
-        />
-        <meta name="theme-color" content="#000000" />
-        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      </head>
       <body
-        className={cn(inter.className, "bg-slate-50	text-sky-700 dark:bg-slate-900 dark:text-slate-300")}
+        className={cn(inter.className, "bg-slate-50 text-sky-700 dark:bg-slate-900 dark:text-slate-300")}
       >
         <AlertBar />
         <NavigationProvider>
           <Navigationbar />
           <ThemeSwitcher />
           <SubpageHeader />
-            <div className="min-h-screen">{children}</div>
+          <div className="min-h-screen">{children}</div>
           <Footer />
         </NavigationProvider>
       </body>
