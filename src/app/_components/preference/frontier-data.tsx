9d05ed73aeea15405b7f69a547ae41bced6bee93
tkokhing <tkokhing@gmail.com>
2025-05-29 19:39:13 +0800

    +2 major re-org, allow dynamic language ch in MDX

--- src/app/_components/preference/frontier-data.tsx
+++ src/app/_components/preference/frontier-data.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import enContent from './public/frontier_data_en.mdx';
-import zhContent from './public/frontier_data_zh.mdx';
-// import enContent from '@/../_frontier_data/frontier_data_en.mdx';
-// import zhContent from '@/../_frontier_data/frontier_data_zh.mdx';
+import enContent from '@/lib/_frontier_data/frontier_data_en.mdx';
+import zhContent from '@/lib/_frontier_data/frontier_data_zh.mdx';
 import DisplayLanguageContent from '../language_handler/language-display';
 
 export default function FrontierData() {
