f61bbd40a2376c388025b805f7e78a612a2eaa62
tkokhing <tkokhing@gmail.com>
2026-01-14 16:06:21 +0800

    Build up notes on more AD, defined more constants for sync

--- src/lib/share/constants.ts
+++ src/lib/share/constants.ts
@@ -6,8 +6,6 @@ export const REPO_NAME = ""; // for future expansion use
 export const T_ANI_GIF_URL = `${LOGO_PATH}/tkokhing.gif`;
 export const TKOKHING_LOGO_SVG_URL = `${LOGO_PATH}/tkokhing.svg`;
 
-
-
 // AD Setup
 
 // export const MS01_IP_EXT = "192.168.118.173";
@@ -20,40 +18,57 @@ export const TKOKHING_LOGO_SVG_URL = `${LOGO_PATH}/tkokhing.svg`;
 // // // EncodedText for SCSHELL
 // export const EncodedText = "encoded_text_for_scshell"
 
-
 // export const MS02_IP_INT = "172.16.242.176";
 // export const MS02_PORT_FORWARD = "4443";
 
 // export const DC01_IP = "172.16.242.162";
 // export const DC01_PORT_FORWARD = "4442";
 
+// ----------------------------  Attacker  ----------------------------
+// export const ATTACKER_IP = "192.168.49.123"; //updated   
 
-export const ATTACKER_IP = "192.168.49.123"; //updated 
+export const ATTACKER_IP = "192.162.118.10"; //Vixx
 export const ATTACKER_PORT = "22";
+
+// ---------------------------- AD Domain Name  ----------------------------
+// export const AD_DOMAIN_NAME = "OSCP.EXAM" //updated
+export const AD_DOMAIN_NAME = "CAR.TOON" //mine
+
+// ---------------------------- Client01  aka MS01  ----------------------------
 
 // // Client01 (aka MS01) Browser Viewing
 // // - Internal (172.16.242.173)
+// export const MS01_username = "r.andrews";  // updated
+// export const MS01_password = "BusyOfficeWorker890"; //updated
+export const MS01_username = "his_username";  // updated
+export const MS01_password = "his_password"; //updated
 
-export const AD_DOMAIN_NAME = "OSCP.EXAM" //updated
+// export const MS01_IP_EXT = "192.168.123.206";  //updated
+// export const MS01_IP_INT = "172.16.123.206"; // updated
+export const MS01_IP_EXT = "192.168.118.173"; //Vixx
+export const MS01_IP_INT = "172.16.242.173";  //Vixx
 
-export const MS01_username = "r.andrews";  // updated
-export const MS01_password = "BusyOfficeWorker890"; //updated
-
-export const MS01_IP_EXT = "192.168.123.206";  //updated
-export const MS01_IP_INT = "172.16.123.206"; // updated
 export const MS01_FORWARD_PORT_1 = "80";   // To download <u>PowerCat</u>
 export const MS01_FORWARD_PORT_2 = "4444"; // To open <u>reverse shell</u>
 
 
+// ---------------------------- WEB01  aka MS02  ----------------------------
 // // WEB01 (aka MS02) Terminal
 // // - Internal (172.16.242.176)
 //updated
-export const MS02_IP_INT = "172.16.123.202"; //updated
+// export const MS02_IP_INT = "172.16.123.202"; //updated
+export const MS02_IP_INT = "172.16.242.176"; //Vixx
 export const MS02_PORT_FORWARD = "4443";
 
+
+// ---------------------------- DC01  aka DC01  ----------------------------
 //updated
-export const DC01_IP = "172.16.123.200"; //updated
+// export const DC01_IP = "172.16.123.200"; //updated
+export const DC01_IP = "172.16.242.162"; //Vixx
 export const DC01_PORT_FORWARD = "4442";
+
+
+// ---------------------------- AD Attacker  ----------------------------
 
 export const AD_ATTACKER_IP = ATTACKER_IP;
 export const AD_ATTACKER_LISTEN_PORT_1 = "80";  // To facilitate pulling of <u>PowerCat</u>
@@ -72,14 +87,16 @@ export const RUNNING_SPN_PASSWORD = "running_spn_pas$word";
 export const EncodedText = "please_update_encoded_text_for_scshell"
 
 
+// ---------------------------- VICTIM  ----------------------------
 // For all sites VICTIM TAGGING  192.168.123.110
 // export const VICTIM1_IP = MS01_IP_EXT;
-export const VICTIM1_IP = "192.168.123.110";
+// export const VICTIM1_IP = "192.168.123.110";
+export const VICTIM1_NAME = MS01_username;
+export const VICTIM1_PASSWORD = MS01_password;
+export const VICTIM1_IP = MS01_IP_EXT;
 export const VICTIM1_PORT = MS01_FORWARD_PORT_2;
 
-
-
-// 
+// Scan on MS01
 // PORT     STATE SERVICE       REASON
 // 135/tcp  open  msrpc         syn-ack ttl 127
 // 139/tcp  open  netbios-ssn   syn-ack ttl 127
